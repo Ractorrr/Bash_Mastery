0501e216c7a9851b5e66f9aed9d29e7a24eb5281
Ractor <142397305+Ractorrr@users.noreply.github.com>
2023-11-18 21:25:48 +0530

    Read Statement (Welcome Words).sh

--- Read Statement (Welcome Words)/Read Statement (Welcome Words).sh
+++ Read Statement (Welcome Words)/Read Statement (Welcome Words).sh
@@ -1,8 +1,31 @@
 #!/bin/bash
 
-read -p "Please Enter your first name: " name
-read -p "Please Enter your University name: " un
-read -p "Please Enter your Age: " age
-read -s -p "Please Enter you password: " pass
+# Author: Ractor
+# Created: 10-July-23
+# Last Modified: 18-Nov-23
 
-echo "Hello $name, Welcome to $un, you are $age so you are in $age's group, so you entered you secret password and buddy it is visible to everyone '$pass'"
+# Description:
+# This script is a simple interactive timer project that showcases the use of GETOPTS and WHILE loop.
+# It prompts the user for their first name, university name, age, and a password.
+
+# Usage:
+# Timer.sh
+
+# Prompt the user for their first name
+read -p "Please enter your first name: " name
+
+# Prompt the user for their university name
+read -p "Please enter your university name: " un
+
+# Prompt the user for their age
+read -p "Please enter your age: " age
+
+# Prompt the user for a password without displaying it
+read -s -p "Please enter your password: " pass
+
+# Display a welcome message with the provided information
+echo "Hello $name, welcome to $un! You are $age years old, so you are in the $age's group. 
+Your secret password, '$pass', is visible to everyone."
+
+# Note: It's recommended not to display passwords, even for educational purposes. 
+# In real-world scenarios, passwords should be handled securely.
